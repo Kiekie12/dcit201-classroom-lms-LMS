c5b9b530891c23161dc8bca65cf105f27cd6e6b8
Kiekie12 <sackeye248@gmail.com>
2024-12-18 23:41:47 +0000

    Create Library.java

---
+++ app/src/main/java/org/example/Library.java
@@ -0,0 +1,19 @@
+ class Library{
+        private List<Book>books;
+
+        public Library(){
+            this.books = new ArrayList<>();
+        }
+        public Book findBook(Book book){
+            for(book:book) = books;{
+                if 
+                (book.getTitle().equalsIgnoreCase(title))
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+        public void addBook(Book book){
+            books.add(books);
+        }
