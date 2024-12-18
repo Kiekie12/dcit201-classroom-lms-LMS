7dcd52f3d54654581bf57a1514d95563fb7b2eba
Kiekie12 <sackeye248@gmail.com>
2024-12-18 23:38:31 +0000

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,21 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+                Library library = new Library();
+
+               library.addBook(new Book("2003", "Finster"));
+        library.addBook(new Book("The Child Called It", "Myster Robbins"));
+        library.addBook(new Book("Youth mana", "Steph Quinn"));
+
+                Patron Kiekie = new Patron("Kiekie");
+        Patron Lizzie = new Patron("Lizzie");
+
+             System.out.println("Finster borrows '2003': " + Finster.borrowBook(library, "2003"));
+        System.out.println("George borrows '2015': " + George.borrowBook(library, "2015"));   
+
+               System.out.println("Finster's borrowed books: " + FInster.getBorrowedBooks().size());
+              System.out.println("Finster returns '2003': " + Finster.returnBook(library, "2003")); 
+        System.out.println("Available books in the library: " + library.getAvailableBooks().size()); 
     }
 }
