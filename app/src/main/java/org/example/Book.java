5adb5fec9f79f15d96a8e189b4a5ff5e25f4ce5e
Kiekie12 <sackeye248@gmail.com>
2024-12-18 23:41:05 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -25,25 +25,7 @@ public class Book{
         isBorrowed = false;
     }
 
-    class Library{
-        private List<Book>books;
-
-        public Library(){
-            this.books = new ArrayList<>();
-        }
-        public Book findBook(Book book){
-            for(book:book) = books;{
-                if 
-                (book.getTitle().equalsIgnoreCase(title))
-                {
-                    return book;
-                }
-            }
-            return null;
-        }
-        public void addBook(Book book){
-            books.add(books);
-        }
+   
     }
 
     class Patron{
